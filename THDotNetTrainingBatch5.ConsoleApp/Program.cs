09f08497a24730401f9f644aab88934902027286
KoTerryHtay <luaye1990@gmail.com>
2026-07-23 21:22:13 +0630

    feat: ado.net create, read, edit and update

--- THDotNetTrainingBatch5.ConsoleApp/Program.cs
+++ THDotNetTrainingBatch5.ConsoleApp/Program.cs
@@ -1,6 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+using THDotNetTrainingBatch5.ConsoleApp;
+
 Console.WriteLine("Hello, World!");
 //Console.ReadLine();
-Console.ReadKey();
 
 // md => markdown
+// C# => Database
+
+// ADO.NET
+// Dapper
+// EFCore / Entity Framework
+
+// nuget
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+adoDotNetExample.Edit();
+
+
+Console.ReadKey();
