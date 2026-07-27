86a8e76ac267d6ab876add7d4c9914f9f08ab95d
KoTerryHtay <luaye1990@gmail.com>
2026-07-27 14:40:19 +0630

    feat: AdoDotNetService, DapperService

---
+++ THDotNetTrainingBatch5.Shared/AdoDotNetService.cs
@@ -0,0 +1,76 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace THDotNetTrainingBatch5.Shared
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connectionString;
+
+        public AdoDotNetService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable Query(string query, params SqlParameterModel[] sqlParameterModel)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if (sqlParameterModel is not null)
+            {
+
+                foreach (var sqlParameter in sqlParameterModel)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            return dt;
+        }
+
+        public int Execxute(string query, params SqlParameterModel[] sqlParameterModel)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            if (sqlParameterModel is not null)
+            {
+
+                foreach (var sqlParameter in sqlParameterModel)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            var result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            return result;
+        }
+    }
+
+    public class SqlParameterModel
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        public SqlParameterModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+
+        }
+    }
+}
---
+++ THDotNetTrainingBatch5.Shared/DapperService.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THDotNetTrainingBatch5.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> Query<T>(string query, object? param = null)
+        {
+            //Console.WriteLine("Query is: " + query);
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var lst = db.Query<T>(query, param).ToList();
+            return lst;
+
+        }
+
+        public T QueryFirstOrDefault<T>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var item = db.QueryFirstOrDefault<T>(query, param);
+            return item;
+
+        }
+
+        public int Execute(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            int result = db.Execute(query, param);
+            return result;
+        }
+    }
+}
